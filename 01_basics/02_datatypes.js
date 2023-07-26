f650de582df8515fd0452618dd4c4c192848dc9f
Aryan Tyagi <126264247+aryan-tyagi2307@users.noreply.github.com>
2023-07-26 17:41:56 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version iss keyword se sara code JS ke nye version ki trah treat hoga
+
+// alert("hello"); // we are using node, not browser as it has different syntax in node
+
+
+/*
+
+                        DATATYPES in JS
+
+       Type	          typeof return value	         Object wrapper
+
+    1. Null	                "object"	                N/A
+    2. Undefined            "undefined"	                N/A
+    3. Boolean	            "boolean"	                Boolean
+    4. Number	            "number"	                Number
+    5. BigInt	            "bigint"	                BigInt
+    6. String	            "string"	                String
+    7. Symbol	            "symbol"	                Symbol
+
+*/
+
+console.log(typeof "aryan");
+console.log(typeof true);
+console.log(typeof 1);
+console.log(typeof undefined);
+console.log(typeof null);
+// console.log(typeof bigint);
+// console.log(typeof symbol);
