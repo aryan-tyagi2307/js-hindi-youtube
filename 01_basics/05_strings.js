f9dcabe750539025498345c3abec26ac39edb435
Aryan Tyagi <126264247+aryan-tyagi2307@users.noreply.github.com>
2023-07-28 15:29:12 +0000

    strings as objects

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,9 @@
+const gameName = "gta5";
+const myName = "Aryan Tyagi";
+console.log(`Hello my name is ${myName} and is love to play ${gameName}`); //morden syntax for console log
+console.log(typeof gameName);
+console.log(gameName[1]);
+
+const name = new String('AryanTyagi'); // stores string in form of object (non primitive datatype) typeof => object
+console.log(typeof name);
+
