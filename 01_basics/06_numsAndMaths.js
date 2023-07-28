22cd2eed01036dfc66a2913ed38d76bae773ae19
Aryan Tyagi <126264247+aryan-tyagi2307@users.noreply.github.com>
2023-07-28 16:50:48 +0000

    number as object and  math object

--- 01_basics/06_numsAndMaths.js
+++ 01_basics/06_numsAndMaths.js
@@ -3,9 +3,9 @@ const num = 500;
 const num2 = new Number(16);
 console.log(num2);  
 
-const max = 10;
-const min = 20;
-console.log(Math.floor(Math.random() * (max - min +1)) + min);
+const max0 = 10;
+const min1 = 20;
+console.log(Math.floor(Math.random() * (max0 - min1 +1)) + min1);
 
 
 // ********************** hitesh's code ************************
