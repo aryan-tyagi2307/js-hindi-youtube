c70904271556650c01c8ee222bf63cbaf18cbefc
Aryan Tyagi <126264247+aryan-tyagi2307@users.noreply.github.com>
2023-07-30 23:31:11 +0530

    Update 04_objects.js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -82,3 +82,8 @@ console.log(instructor);
 
 // studied about json(javascript object notation) and destructuring of objects 
 // as well as about API api is called in form of json with objects and arrays
+
+
+
+
+
