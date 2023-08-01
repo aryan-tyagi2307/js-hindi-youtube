9bd69adac8beae89687e37052d279ffa9e7e7f40
Aryan Tyagi <126264247+aryan-tyagi2307@users.noreply.github.com>
2023-08-01 16:30:52 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,12 @@
+
+if (true) {
+    let a=10;
+    const b=20;
+    var c=30;
+}
+
+// console.log(a); as it is in block scope i.e. it is declared under if statement
+
+// console.log(b); as it is also in block scope i.e. it is declared under if statement
+
+console.log(c); // it does not follow scoping
