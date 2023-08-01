344813a880c0dc473bfa61cdafb9953c401a7ffb
Aryan Tyagi <126264247+aryan-tyagi2307@users.noreply.github.com>
2023-08-01 17:06:50 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -10,3 +10,26 @@ if (true) {
 // console.log(b); as it is also in block scope i.e. it is declared under if statement
 
 console.log(c); // it does not follow scoping
+
+
+if (true) {
+    const username = "Aryan";
+    if (true) {
+        const website = "Youtube";
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+function one() {
+    const user = "Aryan";
+    function two() {
+        const browser = "Chrome";
+        console.log(browser );
+    }
+    console.log(two());
+    console.log(user);    
+}
+console.log(one());
