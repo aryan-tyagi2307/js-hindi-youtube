1bffd79b5b30048440c089f5ff69bc50201e0340
Aryan Tyagi <126264247+aryan-tyagi2307@users.noreply.github.com>
2023-08-02 17:45:19 +0000

    controlFlow

--- 04_controlFlow/one.js
+++ 04_controlFlow/one.js
@@ -6,3 +6,10 @@ if(isLoggedIn){
 }
 
 //  <, >, >=, <=, != ,== ,=== for strict check of datatypes
+
+const num = 500;
+
+if(num>400){
+    const name = `aryan`;
+    console.log(`Number is greater than 400, ${name}`);
+}
