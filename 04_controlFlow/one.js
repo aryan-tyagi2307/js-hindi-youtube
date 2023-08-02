972f2c697063fa99063a4a96c99467ee48c2d4b5
Aryan Tyagi <126264247+aryan-tyagi2307@users.noreply.github.com>
2023-08-02 17:37:24 +0000

    controlFlow

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,8 @@
+const isLoggedIn = true;
+const name = "Aryan Tyagi";
+
+if(isLoggedIn){
+    console.log(`Yes user logged in, ${name}`);
+}
+
+//  <, >, >=, <=, != ,== ,=== for strict check of datatypes
