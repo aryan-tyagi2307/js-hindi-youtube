3011e4172a875ffbea05d53654828e9a7490ca90
Aryan Tyagi <126264247+aryan-tyagi2307@users.noreply.github.com>
2023-08-05 18:02:35 +0000

    switch

--- 04_controlFlow/switch.js
+++ 04_controlFlow/switch.js
@@ -23,3 +23,23 @@ switch (month) {
     default:
         break;
 }
+
+const monthInString = `feb`;
+
+switch (monthInString) {
+    case "jan":
+        console.log(`January`);
+        break;
+    case "feb":
+        console.log(`February`);
+        break;
+    case "march":
+        console.log(`March`);
+        break;
+    case "april":
+        console.log(`April`);
+        break;
+
+    default:
+        break;
+}
